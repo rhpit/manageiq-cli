ed2f9ba672e2d27e054ab1ffe3cb3b93a10ce540
Ryan Williams <3375653+rywillia@users.noreply.github.com>
2017-12-06 12:14:35 -0500

    define entry points section within setup.cfg
    Reverted the setup.py back to its normal state to maintain the minimal
    data required by pbr. Defines the console script to be executed within
    the setup.cfg following pbr standards.

--- setup.py
+++ setup.py
@@ -1,16 +1,8 @@
 #!/usr/bin/env python
 
-from setuptools import find_packages, setup
-
-from miqcli.constants import PACKAGE, VERSION
+from setuptools import setup
 
 setup(
-    name=PACKAGE,
-    description='ManageIQ command line interface',
-    version=VERSION,
-    packages=find_packages('.'),
-    install_requires=['click==6.7'],
     setup_requires=['pbr'],
     pbr=True,
-    entry_points={'console_scripts': ['{0}={0}.cli.main:cli'.format(PACKAGE)]}
 )
