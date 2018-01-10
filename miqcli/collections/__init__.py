8d44699374577c4280669cac1f667aa8beb5517b
Tiago M. Vieira <tmoreira@redhat.com>
2018-01-10 11:05:40 -0500

    improve CLientAPI constructor
    By improving the constructor, we protect the class to misbehave
    in case settings is changed by an external entity. This commit
    also improves the connect function by breaking it down in smaller
    functions that do one thing only.
    
    In addition to this commit, it also disable urllib3 warnings about
    connecting to an https instance with an invalid certificate.
    
    Closes #45

--- miqcli/collections/__init__.py
+++ miqcli/collections/__init__.py
@@ -24,8 +24,6 @@ class Collection(ClientAPI):
     Main option is to save the settings
     """
 
-    _settings = None
-
     def __init__(self, settings):
         """
         :param settings: MIQ settings
