1ee97182fbfcf697fde96a3becfa917f2b46a8f5
Vimal Patel <vipatel@redhat.com>
2018-02-16 11:42:28 -0500

    adding support for request tasks and updating example

--- example/osp_provision_example.py
+++ example/osp_provision_example.py
@@ -19,12 +19,19 @@ import ast
 import json
 from time import sleep
 
-# create a client object
-# pass in credentials, empty {} means it will use the default credentials
-client = Client({})
-print(client)
 
-# 1. pass input to a script to gen a floating ip
+# create a client object
+# pass in config, if empty {} means it will use the default credentials
+config = {
+    'username': 'admin',
+    'password': 'smartvm',
+    'url': 'https://localhost:8443',
+    'enable_ssl_verify': False
+}
+
+client = Client(config)
+
+# 1. Gather the input payload data
 payload_file = "openstack_provision_ex.json"
 
 with open(payload_file) as f:
@@ -34,25 +41,37 @@ with open(payload_file) as f:
         print("Error loading json data: {0}".format(e))
         raise SystemExit(1)
 
-print(payload_data)
 if "fip_pool" in payload_data and payload_data["fip_pool"]:
     # 2. the script will first make an automate request to get a floating ip
     # (cli call to automate get_floating_ip) this call returns an id to watch
+    # if the "fip_pool" id is set, if not it will skip this step.
     client.collection = "automation_requests"
     req_id = client.collection.create(
         method='gen_floating_ip', payload=str(payload_data), payload_file=None)
-    print(req_id)
-
 
     # 3. the script will keep querying automate_request for the status to be
-    #  finished, once finished, it will query for the floating ip id. If there was
-    #  an error, the script will display the error message and exit.
+    #  active, once active, it will monitor the spawned request task.
     done = False
     while not done:
         result = client.collection.status(req_id)
-        if result.request_state == "finished":
+        if result.request_state == "active":
             done = True
         sleep(5)
+
+    # 4. the script will keep querying request task and wait for the state to
+    #  be finished, once finished, it will query for the floating ip id. If
+    #  there was an error, the script will display the error message and exit.
+    client.collection = "request_tasks"
+    done = False
+    while not done:
+        result = client.collection.status(req_id)
+        if result.state == "finished":
+            done = True
+        sleep(5)
+
+    # Task is complete, go back to the automation request to get the output
+    client.collection = "automation_requests"
+    result = client.collection.status(req_id)
 
     # see the return and options of the automate request
     print("Options from the automate request: {0}".format(result.options))
@@ -83,37 +102,39 @@ if "fip_pool" in payload_data and payload_data["fip_pool"]:
 #  provision_request create --provider OpenStack this will return an id
 client.collection = 'provision_requests'
 req_id = client.collection.create('OpenStack', str(payload_data), "")
-print("ID of the provision request: {0}".format(req_id))
+print("ID of the request: {0}".format(req_id))
 
-
-# 6. the script will keep querying the provision_request task for the status
-#  to be finished, once finished, it will return information about the
-#  provisioned machine or display the error message
+# Query the provision request until it is active, then query
+# the spawned request task
 done = False
 while not done:
     result = client.collection.status(req_id)
-    if result.request_state == "finished":
+    if result.request_state == "active":
         done = True
     sleep(5)
 
-if result.status == "Error":
-    # seems like after state is finished it takes a long time to get
-    # the correct error message of the failure.
-    print("Gathering more information on the error, Please Wait.")
-    sleep(90)
+# 6. the script will query the request task until the state is finished,
+#  once finished, it will return information about the provisioned machine
+#  or display the error message
+client.collection = "request_tasks"
+done = False
+while not done:
     result = client.collection.status(req_id)
-    req_state = result.request_state
+    if result.state == "finished":
+        done = True
+    sleep(5)
+
+# 7. Report the floating ip address back to the user if there are no errors
+if result.status == "Error":
+    req_state = result.state
     req_status = result.status
     print("State: {0} and Status: {1}".format(req_state, req_status))
     print("Message: {0}".format(result.message))
-
-# 7. Report the floating ip address back to the user if there are no errors
-if result.status != "Error":
-    # need an extra sleep to make sure floating ip is added to
-    # the instance after provisioning
-    sleep(10)
+else:
+    # Get the data back from the provision_request
+    client.collection = "provision_requests"
+    result = client.collection.status(req_id)
     vm_name = payload_data["vm_name"]
-
     try:
         client.collection = "instances"
         instances = client.collection.query(inst_name=vm_name,
