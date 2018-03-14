9926b41e9feaefdc8f920e852ddf7a4ac60c40a7
Stephen Matula <smatula@redhat.com>
2018-03-14 14:09:09 -0400

    add cound to get_floating_ip automate script

--- Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/get_floating_ip.rb
+++ Automate-Datastore/CF_MIQ_CLI/General/Methods.class/__methods__/get_floating_ip.rb
@@ -5,6 +5,7 @@
 # Input:
 # cloud_tenant_id (integer) required = Cloud tenant id
 # cloud network_id (integer) required = Cloud network id
+# count (integer) optional = Number of Floating IPs requesting
 # 
 # Returned list of ips and ids
 
@@ -56,12 +57,22 @@ begin
     return array
   end
 
+  if ($evm.object['count'].nil? or $evm.object['count'].blank?)
+      count = 1
+  else
+      count = Integer($evm.object['count'])
+  end
+
+  if (count<=0)
+      raise ArgumentError, "count must be positive integer"
+  end
+
   floating_network = $evm.vmdb(:cloud_network).find_by_id($evm.object['cloud_network_id']).name
   log(:info, "Floating Network Name: #{floating_network}")
 
   ip_list = {}
 
-  log(:info, "Create Floating IP")
+  log(:info, "Create Floating IP/IPs")
   require 'fog/openstack'
 
   log(:info, "Object Type: #{$evm.root['vmdb_object_type']}")
@@ -81,10 +92,16 @@ begin
   pool_name = floating_network
   pool_name = list_external_networks(netconn).first["name"] if pool_name.nil?
 
-  log(:info, "Allocating IP from #{pool_name}")
+  log(:info, "Allocating IP/IPs from #{pool_name}")
 
-  address = conn.allocate_address(pool_name).body
-  log(:info, "Allocated #{address['floating_ip'].inspect}")
+  addresses = []
+  while (count > 0)
+      address = conn.allocate_address(pool_name).body
+      log(:info, "Allocated #{address['floating_ip'].inspect}")
+
+      addresses << address
+      count = count - 1
+  end
 
   ext_management_system.refresh
 
@@ -92,36 +109,40 @@ begin
   timer = 600
   new_ip = nil
 
-  while (timer > 0)
-      # Get Floating IP data
-      new_ips = $evm.vmdb(:floating_ip).where(["cloud_network_id = ? and
-                                         cloud_tenant_id = ? and address = ? and
-                                         status = ? and vm_id is NULL",
-                                         $evm.object['cloud_network_id'],
-                                         $evm.object['cloud_tenant_id'],
-                                         address['floating_ip']['ip'],
-                                         "DOWN"])
+  for address in addresses
+      while (timer > 0)
+          # Get Floating IP data
+          new_ips = $evm.vmdb(:floating_ip).where(["cloud_network_id = ? and
+                                             cloud_tenant_id = ? and
+                                             address = ? and status = ? and
+                                             vm_id is NULL",
+                                             $evm.object['cloud_network_id'],
+                                             $evm.object['cloud_tenant_id'],
+                                             address['floating_ip']['ip'],
+                                             "DOWN"])
      
-      # Have data done
-      if new_ips and new_ips.length > 0
-          log(:info, "Time: #{600 - timer} seconds - Number of IPs: #{new_ips.length}")
-          log(:info, "Floating IPs: #{new_ips.inspect}")
-          new_ip = new_ips[0]
-          break
-      end
+          # Have data done
+          if new_ips and new_ips.length > 0
+              log(:info, "Time: #{600 - timer} seconds - Number of IPs: #{new_ips.length}")
+              log(:info, "Floating IPs: #{new_ips.inspect}")
+              new_ip = new_ips[0]
+              break
+          end
       
-      # Wait 5 seconds, count down and continue
-      sleep(5)
-      timer = timer - 5
+          # Wait 5 seconds, count down and continue
+          sleep(5)
+          timer = timer - 5
 
-      # Raise error if taking to long to get data
-      raise 'ERROR: Timeout getting created floating IP information' if timer <=0
+          # Raise error if taking to long to get data
+          raise 'ERROR: Timeout getting created floating IP information' if timer <=0
+      end
+
+      log(:info, "Floating IP: #{address['floating_ip']['ip']} ID: #{new_ip.id}")
+      ip_list[address['floating_ip']['ip']] = new_ip.id 
+
+      log(:info, "ip #{ip_list}")
+
   end
-
-  log(:info, "Floating IP: #{address['floating_ip']['ip']} ID: #{new_ip.id}")
-  ip_list[address['floating_ip']['ip']] = new_ip.id 
-
-  log(:info, "ip #{ip_list}")
   
   # For automation_task set return data. status and return data
   # List of IP's and their IDs
