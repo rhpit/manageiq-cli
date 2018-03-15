f1c832227d3cbdf3a970d5d2710c95c4399eb93e
Tiago M. Vieira <tmoreira@redhat.com>
2018-03-15 09:00:56 -0400

    add test to check collection in CLI is the same as in the appliance
    * create two tox integration envs: for python 2.7 and 3.6
    * both envs generate xunit files

--- tests/integration/test_collections.py
+++ tests/integration/test_collections.py
@@ -13,14 +13,16 @@ class TestCollections(TestCase):
 
         # I want a list of all valid collections from the appliance
         r = requests.get('https://localhost:8443/api', auth=('admin', 'smartvm'), verify=False)
-        self.appliance_collections = set([c['name'] for c in r.json()['collections']])
+        self.appliance_collections = [c['name'] for c in r.json()['collections']]
 
     def tearDown(self):
         del(self.appliance_collections)
 
     def test_collection_representation_from_appliances(self):
+        # test if CLI collections matches appliance collections
         import miqcli.collections
         colpath = os.path.dirname(miqcli.collections.__file__)
-        client_collections = set([name for _, name, _ in pkgutil.iter_modules([colpath])])
-        dif = client_collections.difference(self.appliance_collections)
+        client_collections = [name for _, name, _ in pkgutil.iter_modules([colpath])]
+        assert_equal(len(client_collections), len(self.appliance_collections))
+        dif = [item for item in client_collections if item not in appliance_collections]
         assert_equal(len(dif), 0)
